32d6869ec98ea09ea03ea29f9ddfc0af130bb12a
Ribamar Santos <ribamaar@gmail.com>
2018-06-25 23:59:27 -0300

    Setting HEader component

---
+++ app/components/Header/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { View, TouchableOpacity, Image } from 'react-native';
+import Proptypes from 'prop-types';
+
+import styles from './styles';
+
+const Header = ({ onPress }) => (
+  <View style={styles.container}>
+    <TouchableOpacity style={styles.button} onPress={onPress}>
+      <Image resizeMode="cotain" style={styles.icon} source={require('./images/gear.png')} />
+    </TouchableOpacity>
+  </View>
+);
+
+Header.propTypes = {
+  onPress: Proptypes.func,
+};
+
+export default Header;
