68e5ef03a92fe5279a35c960523bff278afc2f45
Ribamar Santos <ribamaar@gmail.com>
2018-07-11 14:53:38 -0300

    Adding animation

--- app/components/Logo/styles.js
+++ app/components/Logo/styles.js
@@ -4,17 +4,22 @@ import EStyleSheet from 'react-native-extended-stylesheet';
 const imageWidth = Dimensions.get('window').width / 2;
 
 export default EStyleSheet.create({
+  $largeContainerWidth: imageWidth,
+  $largeImageWidth: imageWidth / 2,
+  $smallContainerWidth: imageWidth / 4,
+  $smallImageWidth: imageWidth / 4,
+
   container: {
     alignItems: 'center',
   },
   containerImage: {
     alignItems: 'center',
     justifyContent: 'center',
-    width: imageWidth,
-    height: imageWidth,
+    width: '$largeContainerWidth',
+    height: '$largeContainerWidth',
   },
   image: {
-    width: imageWidth / 2,
+    width: '$largeImageWidth',
   },
   text: {
     fontWeight: '600',
