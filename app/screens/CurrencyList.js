084ad33b456c6154f64156de0621002d47bd42cb
Ribamar Santos <ribamaar@gmail.com>
2018-07-16 15:47:03 -0300

    Doing ListItem

--- app/screens/CurrencyList.js
+++ app/screens/CurrencyList.js
@@ -1,17 +1,31 @@
-import React from 'react';
-import { View, FlatList, StatusBar, Text } from 'react-native';
-
+import React, { Component } from 'react';
+import { View, FlatList, StatusBar } from 'react-native';
+import { ListItem, Separator } from '../components/List';
 import currencies from '../data/currencies';
 
-const CurrencyList = () => (
-  <View style={{ flex: 1 }}>
-    <StatusBar translucent={false} barStyle="default" />
-    <FlatList
-      data={currencies}
-      renderItem={({ item }) => <Text>{item}</Text>}
-      keyExtractor={item => item}
-    />
-  </View>
-);
+const TEMP_CURRENT_CURRENCY = 'CAD';
+
+class CurrencyList extends Component {
+  handlePress = () => {};
+  render() {
+    return (
+      <View style={{ flex: 1 }}>
+        <StatusBar translucent={false} barStyle="default" />
+        <FlatList
+          data={currencies}
+          renderItem={({ item }) => (
+            <ListItem
+              text={item}
+              onPress={this.handlePress}
+              selected={item === TEMP_CURRENT_CURRENCY}
+            />
+          )}
+          keyExtractor={item => item}
+          ItemSeparatorComponent={Separator}
+        />
+      </View>
+    );
+  }
+}
 
 export default CurrencyList;
