0b2d35b39f68d85ad6cd25cf0c9cc83c245d1f67
Ribamar Marcos <ribamaar@gmail.com>
2018-10-17 22:12:22 -0500

    Config saga

--- app/config/store.js
+++ app/config/store.js
@@ -1,11 +1,18 @@
 import { createStore, applyMiddleware } from 'redux';
+import createSagaMiddleware from 'redux-saga';
 import logger from 'redux-logger';
 import reducers from '../reducers';
+import sagas from './sagas';
 
-const middleware = [];
+const sagaMiddleware = createSagaMiddleware();
+
+const middleware = [sagaMiddleware];
 
 if (process.env.NODE_ENV === 'development') {
   middleware.push(logger);
 }
+const store = createStore(reducers, applyMiddleware(...middleware));
 
-export default createStore(reducers, applyMiddleware(...middleware));
+sagaMiddleware.run(sagas);
+
+export default store;
